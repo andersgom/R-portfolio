1a26fde6c5a6e2d9f51e49303ba4bcc97c715332
andersgom <asgomez95@gmail.com>
2022-03-06 15:17:26 +0000

    Change 'R - Data cleaning.R' and README.md

--- R - Data cleaning.R
+++ R - Data cleaning.R
@@ -20,6 +20,10 @@ met_objects_clean <- clean_names(met_objects)
 met_objects_clean$object_id <- as.character(met_objects_clean$object_id)
 met_objects_clean$constituent_id <- as.character(met_objects_clean$constituent_id)
 
+# Create a data frame to further inspect the data
+
+met_summary <- skim_without_charts(met_objects_clean)
+
 
 # Find null values in selected columns
 met_objects_clean %>%
@@ -27,21 +31,25 @@ met_objects_clean %>%
   is.na()%>%
   sum()
 
-# Find duplicates
+# Inspect and fix columns values with white spaces
+
+met_objects_clean$dimensions <- trimws(met_objects_clean$dimensions)
+met_objects_clean$portfolio <- trimws(met_objects_clean$portfolio)
+met_objects_clean$medium <- trimws(met_objects_clean$medium)
+met_objects_clean$credit_line <- trimws(met_objects_clean$credit_line)
 
 
+# Split string values of 'tags' into a list
+
+met_objects_clean$tags <- str_split(met_objects_clean$tags, fixed("|"))
 
 
+#This adds a column that count the number of tags
 
-# Tasks after checking the summary:
-# - Clean column names
-# - Change the data type of 'object_id' and 'constituent_id'
-# 
-# - Delete white spaces in several columns ('portfolio', 'medium', 'dimensions' and 'credit_line')
-# - Split the values in 'tags' into multiple columns
-
-
-
+met_objects_clean <- met_objects_clean%>%
+  mutate(tag_count=lengths(tags))%>%
+  relocate(tag_count, .after = tags)
+# [FIX] NA values counted as 1
 
 
 
--- README.md
+++ README.md
@@ -5,4 +5,6 @@ This repository holds all the code from my personal R projects.
 - Data cleaning: I used the [MAT Open Access CSV dataset](https://github.com/metmuseum/openaccess), downloaded on March 3rd, 2022.
 
     - Topics covered:
-        - 
+        - Packages: `tidyverse`, `dplyr`, `skimr` and `janitor`
+        - Clean column names
+        - Convert data types
