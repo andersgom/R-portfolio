db29b475752f27704d4a860ee58996f239ecc629
andersgom <asgomez95@gmail.com>
2022-03-07 11:58:16 +0000

    Fix the tag column

--- R - Data cleaning.R
+++ R - Data cleaning.R
@@ -5,7 +5,6 @@ library(dplyr)
 library(skimr)
 library(janitor)
 
-
 # Summary of the dataset
 skim_without_charts(met_objects)
 summary(met_objects)
@@ -42,16 +41,3 @@ met_objects_clean$credit_line <- trimws(met_objects_clean$credit_line)
 # Split string values of 'tags' into a list
 
 met_objects_clean$tags <- str_split(met_objects_clean$tags, fixed("|"))
-
-
-#This adds a column that count the number of tags
-
-met_objects_clean <- met_objects_clean%>%
-  mutate(tag_count=lengths(tags))%>%
-  relocate(tag_count, .after = tags)
-# [FIX] NA values counted as 1
-
-
-
-
-
