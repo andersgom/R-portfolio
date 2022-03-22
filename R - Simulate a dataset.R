f4856c4f1a07dd499ace7d9c334d0433511de300
andersgom <asgomez95@gmail.com>
2022-03-22 13:48:49 +0000

    Add script

--- R - Simulate a dataset.R
+++ R - Simulate a dataset.R
@@ -1,1 +1,13 @@
-# Simulating a dataset
+# Dataset simulation
+
+
+n <- 200
+catvar <- c("item1", "item2", "item3")
+boovar <- c(0,1)
+
+col1 <- sample(catvar, size=10, replace=TRUE)
+col2 <- sample(20, n, replace=TRUE)
+col3 <- rnorm(n, mean = 0, sd = 1)
+col4 <- sample(boovar, size=10, replace=TRUE)
+
+data <- cbind(col1, col2, col3, col4)
